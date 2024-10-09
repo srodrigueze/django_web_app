16e0c041e599334b95f29367e946ad33da292111
srodrigueze <rodriguez.santiagoWork@gmail.com>
2024-10-09 01:16:07 -0500

    First advance in the django web project. Starting with templates and stablishing the bases of the project.

---
+++ djangowebapp/base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me!'},
+    {'id':3, 'name':'Frontend developers!'}
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
+
